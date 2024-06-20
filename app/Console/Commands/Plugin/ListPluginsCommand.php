c2b9743b9701bf98b69926c3b896bd7593978be0
Boy132 <mail@boy132.de>
2024-06-20 13:31:07 +0200

    add author to ListPluginsCommand

--- app/Console/Commands/Plugin/ListPluginsCommand.php
+++ app/Console/Commands/Plugin/ListPluginsCommand.php
@@ -13,12 +13,12 @@ class ListPluginsCommand extends Command
 
     public function handle(): void
     {
-        $plugins = Plugin::query()->get(['package', 'class', 'status', 'name', 'panel', 'category']);
+        $plugins = Plugin::query()->get(['package', 'class', 'status', 'name', 'author', 'panel', 'category']);
 
         if ($this->option('format') === 'json') {
             $this->output->write($plugins->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         } else {
-            $this->table(['Package Name', 'Main Class', 'Status', 'Display Name', 'Panel', 'Category'], $plugins->toArray());
+            $this->table(['Package Name', 'Main Class', 'Status', 'Name', 'Author', 'Panel', 'Category'], $plugins->toArray());
         }
 
         $this->output->newLine();
