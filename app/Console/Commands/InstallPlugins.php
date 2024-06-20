8f4f55d51e71bf1acde5dae90d71694cbfc32f76
Boy132 <mail@boy132.de>
2024-06-20 10:25:28 +0200

    add artisan command to (re-) install all plugins

---
+++ app/Console/Commands/InstallPlugins.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Plugin;
+use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
+
+class InstallPlugins extends Command
+{
+    protected $signature = 'p:install-plugins';
+
+    protected $description = 'Installs all plugins via composer require';
+
+    public function handle()
+    {
+        $pluginPackages = Plugin::query()->select('package')->pluck('package')->toArray();
+
+        /** @var Composer $composer */
+        $composer = app(Composer::class);
+        $composer->requirePackages($pluginPackages, false, $this->output);
+    }
+}
