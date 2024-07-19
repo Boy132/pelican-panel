ccd631515f163ec18bf9fb0ccd5604d5b53ac66d
Boy132 <mail@boy132.de>
2024-07-19 09:15:52 +0200

    create plugin model

---
+++ database/migrations/2024_06_17_143810_create_plugins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plugins', function (Blueprint $table) {
+            $table->string('package')->unique();
+            $table->string('class');
+            $table->string('status')->default('enabled');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('author');
+            $table->string('version');
+            $table->string('panel');
+            $table->string('panel_version')->nullable();
+            $table->string('category');
+            $table->string('update_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plugins');
+    }
+};
