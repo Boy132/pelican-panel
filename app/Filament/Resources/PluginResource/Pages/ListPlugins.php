e31c1a6158793d6c74050760d5dbaf849c3c59b5
Boy132 <mail@boy132.de>
2024-07-23 14:26:19 +0200

    use imports

--- app/Filament/Resources/PluginResource/Pages/ListPlugins.php
+++ app/Filament/Resources/PluginResource/Pages/ListPlugins.php
@@ -8,12 +8,15 @@ use App\Filament\Resources\PluginResource;
 use App\Services\Plugins\PluginInstallService;
 use Exception;
 use Filament\Actions;
-use Filament\Forms;
 use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Table;
-use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Columns\TextColumn;
 
 class ListPlugins extends ListRecords
 {
@@ -24,34 +27,34 @@ class ListPlugins extends ListRecords
         return $table
             ->searchable(false)
             ->columns([
-                Tables\Columns\TextColumn::make('name')
+                TextColumn::make('name')
                     ->description(fn (Plugin $record): ?string => (strlen($record->description) > 60) ? substr($record->description, 0, 60).'...' : $record->description)
                     ->icon(fn (Plugin $record): string => $record->isCompatible() ? 'tabler-versions' : 'tabler-versions-off')
                     ->iconColor(fn (Plugin $record): string => $record->isCompatible() ? 'success' : 'danger')
                     ->tooltip(fn (Plugin $record): ?string => !$record->isCompatible() ? 'This Plugin is only compatible with Panel version ' . $record->panel_version . ' but you are using version!' : null)
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('author')
+                TextColumn::make('author')
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('version')
+                TextColumn::make('version')
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('category')
+                TextColumn::make('category')
                     ->badge()
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('status')
+                TextColumn::make('status')
                     ->icon(fn (PluginStatus $state) => $state->icon())
                     ->iconColor(fn (PluginStatus $state) => $state->color())
                     ->sortable(),
             ])
             ->actions([
-                Tables\Actions\ActionGroup::make([
-                    Tables\Actions\Action::make('view')
+                ActionGroup::make([
+                    Action::make('view')
                         ->icon('tabler-eye-share')
                         ->url(fn (Plugin $record): string => 'https://github.com/' . $record->package, true),
-                    Tables\Actions\Action::make('enable')
+                    Action::make('enable')
                         ->icon('tabler-check')
                         ->color('success')
                         ->hidden(fn (Plugin $record) => !$record->isDisabled())
@@ -64,7 +67,7 @@ class ListPlugins extends ListRecords
                                 ->title('Plugin enabled')
                                 ->send();
                         }),
-                    Tables\Actions\Action::make('disable')
+                    Action::make('disable')
                         ->icon('tabler-x')
                         ->color('danger')
                         ->hidden(fn (Plugin $record) => $record->isDisabled())
@@ -77,12 +80,12 @@ class ListPlugins extends ListRecords
                                 ->title('Plugin disabled')
                                 ->send();
                         }),
-                    Tables\Actions\Action::make('update')
+                    Action::make('update')
                         ->icon('tabler-download')
                         ->color('primary')
                         ->hidden(fn (Plugin $record) => !$record->isUpdateAvailable())
                         ->action(fn (Plugin $record) => resolve(PluginInstallService::class)->update($record)),
-                    Tables\Actions\Action::make('uninstall')
+                    Action::make('uninstall')
                         ->icon('tabler-trash')
                         ->color('danger')
                         ->requiresConfirmation()
@@ -93,7 +96,7 @@ class ListPlugins extends ListRecords
             ->emptyStateDescription('')
             ->emptyStateHeading('No Plugins')
             ->emptyStateActions([
-                Tables\Actions\Action::make('install')
+                Action::make('install')
                     ->label('Install Plugin')
                     ->form(fn () => $this->installForm())
                     ->action(fn (array $data) => $this->installAction($data)),
@@ -133,17 +136,17 @@ class ListPlugins extends ListRecords
         return [
             Tabs::make('Tabs')
                 ->tabs([
-                    Tabs\Tab::make('Name')
+                    Tab::make('Name')
                         ->icon('tabler-file-upload')
                         ->schema([
-                            Forms\Components\TextInput::make('name')
+                            TextInput::make('name')
                                 ->label('Plugin name')
                                 ->hint('Enter the full name of the plugin repo, e.g. author/package-plugin'),
                         ]),
-                    Tabs\Tab::make('URL')
+                    Tab::make('URL')
                         ->icon('tabler-world-upload')
                         ->schema([
-                            Forms\Components\TextInput::make('url')
+                            TextInput::make('url')
                                 ->label('Json URL')
                                 ->hint('This URL should point to a single json file that contains the plugin install info.')
                                 ->url(),
