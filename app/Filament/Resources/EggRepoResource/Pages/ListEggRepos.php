1555dfe328e1de2bbf6885e922c720cf1e14549f
Boy132 <mail@boy132.de>
2024-07-26 09:40:05 +0200

    use filter instead of group

--- app/Filament/Resources/EggRepoResource/Pages/ListEggRepos.php
+++ app/Filament/Resources/EggRepoResource/Pages/ListEggRepos.php
@@ -8,7 +8,7 @@ use App\Services\Eggs\Sharing\EggImporterService;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Grouping\Group;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class ListEggRepos extends ListRecords
@@ -18,17 +18,21 @@ class ListEggRepos extends ListRecords
     public function table(Table $table): Table
     {
         return $table
-            ->defaultGroup('repo')
-            ->groups([
-                Group::make('repo'),
-            ])
             ->columns([
+                TextColumn::make('repo')
+                    ->searchable()
+                    ->sortable(),
                 TextColumn::make('name')
                     ->searchable()
                     ->sortable(),
                 TextColumn::make('tags')
                     ->badge()
-                    ->state(fn (EggRepo $eggRepo) => explode('/', $eggRepo->path)),
+                    ->state(fn (EggRepo $eggRepo) => array_unique(explode('/', $eggRepo->path))),
+            ])
+            ->filters([
+                SelectFilter::make('repo')
+                    ->options(fn () => EggRepo::all()->pluck('repo', 'repo')->unique())
+                    ->multiple(),
             ])
             ->actions([
                 Action::make('import')
