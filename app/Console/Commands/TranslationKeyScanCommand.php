b470be5875b365a0b1ab731664fa705347073d00
Boy132 <mail@boy132.de>
2024-10-17 09:51:16 +0200

    add trait for scanner

--- app/Console/Commands/TranslationKeyScanCommand.php
+++ app/Console/Commands/TranslationKeyScanCommand.php
@@ -2,77 +2,20 @@
 
 namespace App\Console\Commands;
 
+use App\Traits\TranslationScannerTrait;
 use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
 
 class TranslationKeyScanCommand extends Command
 {
+    use TranslationScannerTrait;
+
     protected $description = 'Scans for translation keys.';
 
     protected $signature = 'p:scan-translation-keys';
 
-    private const TRANSLATION_METHODS = [
-        '__',
-        'trans',
-        'trans_choice',
-        '@lang',
-        'Lang::get',
-        'Lang::choice',
-        'Lang::trans',
-        'Lang::transChoice',
-        '@choice',
-    ];
-
     public function handle(): void
     {
-        $results = $this->scan([app_path(), resource_path()]);
-        $this->table(['File', 'Key', 'EN Translation'], $results);
-    }
-
-    public function scan(array $paths): array
-    {
-        $results = [];
-
-        foreach (resolve(Filesystem::class)->allFiles($paths) as $file) {
-            if ($file->getExtension() !== 'php') {
-                continue;
-            }
-
-            if (preg_match_all($this->pattern(), $file->getContents(), $matches)) {
-                foreach ($matches[2] as $key) {
-                    if (!empty($key)) {
-                        $trans = trans($key);
-
-                        $results[] = [
-                            'file' => $file->getRelativePathname(),
-                            'key' => $key,
-                            'translation' => $key === $trans ? '' : (is_array($trans) ? implode($trans) : $trans),
-                        ];
-                    }
-                }
-            }
-        }
-
-        return $results;
-    }
-
-    private function pattern(): string
-    {
-        // See https://regex101.com/r/jS5fX0/5
-        return
-            '/' .
-            "[^\w]" . // Must not start with any alphanum or _
-            '(?<!->)' . // Must not start with ->
-            '(' . implode('|', self::TRANSLATION_METHODS) . ')' .// Must start with one of the functions
-            "\(" .// Match opening parentheses
-            "[\r\n|\r|\n]*?" .// Ignore new lines
-            "[\'\"]" .// Match " or '
-            '(' .// Start a new group to match:
-            '.*' .// Must start with group
-            ')' .// Close group
-            "[\'\"]" .// Closing quote
-            "[\r\n|\r|\n]*?" .// Ignore new lines
-            "[\),]" . // Close parentheses or new parameter
-            '/siuU';
+        $results = $this->scanForTranslations([app_path(), resource_path()], 'php');
+        $this->table(['File', 'Key'], $results);
     }
 }
---
+++ app/Traits/TranslationScannerTrait.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Filesystem\Filesystem;
+
+trait TranslationScannerTrait
+{
+    protected const TRANSLATION_METHODS = [
+        '__',
+        'trans',
+        'trans_choice',
+        '@lang',
+        'Lang::get',
+        'Lang::choice',
+        'Lang::trans',
+        'Lang::transChoice',
+        '@choice',
+    ];
+
+    protected function scanForTranslations(string|array $paths, string|array|null $extensionFilter = null): array
+    {
+        $pattern = '/' .
+                    "[^\w]" . // Must not start with any alphanum or _
+                    '(?<!->)' . // Must not start with ->
+                    '(' . implode('|', self::TRANSLATION_METHODS) . ')' .// Must start with one of the functions
+                    "\(" .// Match opening parentheses
+                    "[\r\n|\r|\n]*?" .// Ignore new lines
+                    "[\'\"]" .// Match " or '
+                    '(' .// Start a new group to match:
+                    '.*' .// Must start with group
+                    ')' .// Close group
+                    "[\'\"]" .// Closing quote
+                    "[\r\n|\r|\n]*?" .// Ignore new lines
+                    "[\),]" . // Close parentheses or new parameter
+                    '/siuU';
+
+        $results = [];
+
+        foreach (app(Filesystem::class)->allFiles($paths) as $file) {
+            if ((is_string($extensionFilter) && $file->getExtension() !== $extensionFilter) || (is_array($extensionFilter) && !in_array($extensionFilter, $file->getExtension()))) {
+                continue;
+            }
+
+            if (preg_match_all($pattern, $file->getContents(), $matches)) {
+                foreach ($matches[2] as $key) {
+                    if (!empty($key)) {
+                        $results[] = [
+                            'file' => $file->getRelativePathname(),
+                            'key' => $key,
+                        ];
+                    }
+                }
+            }
+        }
+
+        return $results;
+    }
+}
