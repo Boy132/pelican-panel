4fbc729ae228bad126c4150f1ee5c73fcf920268
Boy132 <mail@boy132.de>
2024-10-17 11:53:51 +0200

    refactor event name of activity log to prevent wrong translation scans

--- app/Transformers/Api/Client/ActivityLogTransformer.php
+++ app/Transformers/Api/Client/ActivityLogTransformer.php
@@ -95,8 +95,7 @@ class ActivityLogTransformer extends BaseClientTransformer
             return false;
         }
 
-        $str = trans('activity.' . str_replace(':', '.', $model->event));
-        preg_match_all('/:(?<key>[\w.-]+\w)(?:[^\w:]?|$)/', $str, $matches);
+        preg_match_all('/:(?<key>[\w.-]+\w)(?:[^\w:]?|$)/', $model->eventName, $matches);
 
         $exclude = array_merge($matches['key'], ['ip', 'useragent', 'using_sftp']);
         foreach ($model->properties->keys() as $key) {
