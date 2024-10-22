5638ac51bc8cb43fd3607cd1186beb516e8696a5
Boy132 <mail@boy132.de>
2024-10-22 09:46:49 +0200

    run pint & fix phpstan

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets');
 
-        app(PluginService::class)->loadPanelPlugins($panel);
+        app(PluginService::class)->loadPanelPlugins(app(), $panel); // @phpstan-ignore-line
 
         return $panel;
     }
