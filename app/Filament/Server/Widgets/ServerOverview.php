85fecdce8eda5a2e20395c9fbba8d096498736c6
Boy132 <mail@boy132.de>
2024-12-05 10:02:18 +0100

    add player count to server overview

--- app/Filament/Server/Widgets/ServerOverview.php
+++ app/Filament/Server/Widgets/ServerOverview.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Server\Widgets;
 
+use App\Enums\QueryType;
 use App\Models\Server;
+use App\Services\Servers\ServerQueryService;
 use Carbon\CarbonInterface;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -16,12 +18,20 @@ class ServerOverview extends StatsOverviewWidget
 
     protected function getStats(): array
     {
-        return [
+        $stats = [
             Stat::make('Name', $this->server->name)
                 ->description($this->server->description),
             Stat::make('Status', $this->status()),
             Stat::make('Address', $this->server->allocation->address),
         ];
+
+        if ($this->server->egg->query_type !== QueryType::None) {
+            // @phpstan-ignore-next-line
+            $queryData = app(ServerQueryService::class)->normalize()->handle($this->server);
+            $stats[] = Stat::make('Players', !empty($queryData) ? $queryData['players']['current'] . ' / ' . $queryData['players']['max'] . ' Players' : 'Offline');
+        }
+
+        return $stats;
     }
 
     private function status(): string
