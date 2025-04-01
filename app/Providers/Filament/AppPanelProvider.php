6a04dc4695a57eae9218d037bc1b65992d858ec5
Boy132 <mail@boy132.de>
2025-04-01 14:52:42 +0200

    load plugins on all panels

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Services\Helpers\PluginService;
 use Filament\Facades\Filament;
 use Filament\Navigation\MenuItem;
 use Filament\Panel;
@@ -10,7 +11,7 @@ class AppPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        return parent::panel($panel)
+        $panel = parent::panel($panel)
             ->id('app')
             ->default()
             ->breadcrumbs(false)
@@ -24,5 +25,9 @@ class AppPanelProvider extends PanelProvider
                     ->visible(fn () => auth()->user()->canAccessPanel(Filament::getPanel('admin'))),
             ])
             ->discoverResources(in: app_path('Filament/App/Resources'), for: 'App\\Filament\\App\\Resources');
+
+        app(PluginService::class)->loadPanelPlugins(app(), $panel); // @phpstan-ignore-line
+
+        return $panel;
     }
 }
