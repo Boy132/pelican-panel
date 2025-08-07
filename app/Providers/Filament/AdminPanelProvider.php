89e3da1e24f6fb44981d0dbd968fd80c5000201a
Boy132 <mail@boy132.de>
2025-08-07 08:41:23 +0200

    cleanup panel providers

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers\Filament;
 
-use App\Facades\Plugins;
 use App\Filament\Pages\Auth\EditProfile;
 use Filament\Navigation\MenuItem;
 use Filament\Navigation\NavigationGroup;
@@ -12,7 +11,7 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        $panel = parent::panel($panel)
+        return parent::panel($panel)
             ->id('admin')
             ->path('admin')
             ->homeUrl('/')
@@ -38,9 +37,5 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets');
-
-        Plugins::loadPanelPlugins($panel);
-
-        return $panel;
     }
 }
